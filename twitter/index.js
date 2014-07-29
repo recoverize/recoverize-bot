9b08d43a71bafc0528bef9df5a8e2932764265c2
Dane Grant <danecando@gmail.com>
2014-07-28 23:47:43 -0400

    initial commit

---
+++ twitter/index.js
@@ -0,0 +1,8 @@
+var Twit = require('twit');
+
+exports.twitter = new Twit({
+    consumer_key: 'tJgQbMgfNIedrrYaDD0CBTQC1',
+    consumer_secret: 'nJAsyhL65B0NNKllw2fcN3YsTui5aJXvAkh8zmrO3vUeHMU8Ce',
+    access_token: '1733468634-SAtGbzuQJHgJEonioZzffzxI6Sxj4V7X2cm3mkV',
+    access_token_secret: 'PFB3r4p6x1g0CDOcg6RoACn0XYSLa3ZFqpH0kWHIj2yW8'
+});
