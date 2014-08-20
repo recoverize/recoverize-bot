13fa8db6c5b06fe701b315fdadd515b068046915
Dane Grant <danecando@gmail.com>
2014-08-20 19:03:24 -0400

    more promos

--- twitter/index.js
+++ twitter/index.js
@@ -15,7 +15,6 @@ var account = require('./events/account');
 // Load scheduled events
 var tweets = require('./schedule/tweets');
 
-
 // Do automated interactions
 var tags = ['#justfortoday', '#12steps', '#bigbook', '#dailyreflections', '#sobriety', '#xa', '#affirmation', '#nohangovers', '#another24', '#thenaway', '#aahumor', '#recoveryispossible'];
 
